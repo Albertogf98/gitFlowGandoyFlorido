843a80c8034f69cb51107234b55126ec637b41d7
Albertogf98 <alberto1998adoratrices@gmail.com>
2021-01-04 16:40:30 +0100

    C8(graph_employeeGandoyFlorido) último commit de la rama graph_employeeGandoyFlorido

--- src/views/view.js
+++ src/views/view.js
@@ -1,3 +1,12 @@
 class View {
-    constructor() {}
+    constructor() {
+        this.input = document.getElementById('inputTxt');
+        this.button = document.getElementById('butClick');
+    }
+
+    bindButton(handler) {
+        this.button.addEventListener('click', () => {
+            handler(this.input.value);
+        });
+    }
 }

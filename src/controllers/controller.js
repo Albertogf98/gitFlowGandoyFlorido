c14f0163cb264c2bc853f447e09ec3542dfbdd58
Albertogf98 <alberto1998adoratrices@gmail.com>
2021-01-04 16:55:53 +0100

    C9(developGandoyFlorido) integración de la rama graph_employee en develop y también se ha eliminado dicha rama.

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,5 +2,9 @@ class Controller {
     constructor(view, service) {
         this.view  = view;
         this.service = service;
+
+        this.view.bindButton(this.handler);
     }
+
+    handler = value => this.service.getAlert(value);
 }
